337d8a7b28981524f5d196bdabdc24efed065934
Rodrigw07 <rodrigdx07@gmail.com>
2025-10-16 15:19:36 -0300

    Add files via upload

---
+++ meus gastos.py
@@ -0,0 +1,22 @@
+
+meu_salario = 6.000
+
+# gastos
+
+
+
+luz = 40
+
+agua = 56
+
+gas = 110
+
+compras_do_mes = 268
+
+internet = 80
+
+netflix = 100
+
+sobra = (meu_salario - luz - agua - gas - compras_do_mes - internet - netflix)
+
+print(sobra)
